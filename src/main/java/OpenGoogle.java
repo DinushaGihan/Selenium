58d3cc5a694bfa8a963595d94b5529ee8350ffb5
Dinusha Gihan <116588856+DinushaGihan@users.noreply.github.com>
2026-03-10 18:37:55 +0530

    selenium exercises

---
+++ src/main/java/OpenGoogle.java
@@ -0,0 +1,13 @@
+public class OpenGoogle {
+
+    public static void main(String[] args) {
+        //open browser using System.setProperty():
+        System.setProperty(" "," ");
+
+        //open browser using WebDriverManager
+
+
+        //open browser using SeleniumManager
+
+    }
+}
