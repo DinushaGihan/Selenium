e863fb777a4c689cfa047bc084734b3bb8675c5e
Dinusha Gihan <116588856+DinushaGihan@users.noreply.github.com>
2026-03-10 21:45:57 +0530

    Launch Chrome browser using 3 different ways

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,11 +1,18 @@
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OpenGoogle {
 
     public static void main(String[] args) {
-        //open browser using System.setProperty():
-        System.setProperty(" ","xxxxx ");
+        //***************open browser using System.setProperty():**********
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\dasan\\Downloads\\Driver\\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
 
-        //open browser using WebDriverManager
-
+        //**********open browser using WebDriverManager**********
+        //WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
 
         //open browser using SeleniumManager
 
