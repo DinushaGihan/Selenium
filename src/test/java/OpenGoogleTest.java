df498cfe41217763b7b723caec61117f0a55ef32
Dinusha Gihan <116588856+DinushaGihan@users.noreply.github.com>
2026-03-14 09:32:40 +0530

    open google, type colombo and press enter

--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -9,5 +11,6 @@ public class OpenGoogleTest {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+        driver.findElement(By.id("APjFqb")).sendKeys("colombo" + Keys.ENTER);
     }
 }
